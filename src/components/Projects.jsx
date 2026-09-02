927f968cfd6a07491be229bcc3412041a2d9aa1d
Shaik Mahammad Sajid <maraballikwt@gmail.com>
2026-09-03 00:05:28 +0530

    Reorder project data and update links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,27 @@ import '../styles/projects.css';
 
 export default function Projects() {
   const projectsData = [
+
 {
   id: 1,
+  title: 'FixMyVillage',
+  description: 'A full-stack village issue management platform with a Django-based backend that enables villagers to report public issues such as garbage, water leakage, drainage, electricity, and road damage. Implements role-based access, complaint management, REST APIs, database operations, status tracking, and email notifications.',
+  tech: [
+    'Python',
+    'Django',
+    'Django REST Framework',
+    'MySQL',
+    'React.js',
+    'CSS'
+  ],
+  repoLink: 'https://github.com/skmsajid/fixmyvillage',
+  highlights: [
+    'Complaint Management',
+    'REST API Development',
+    'Role-Based Access'  ]
+},
+    {
+  id: 2,
   title: 'Friends Cart',
   description: 'A backend-focused e-commerce application built with Django and MySQL, featuring product management, shopping cart functionality, coupon validation, product reviews and ratings, and secure user authentication.',
   tech: ['Python', 'Django', 'MySQL', 'HTML', 'CSS', 'JavaScript'],
@@ -13,27 +32,6 @@ export default function Projects() {
     'Coupon System',
     'User Authentication',
     'Reviews & Ratings'
-  ]
-},
-{
-  id: 2,
-  title: 'FixMyVillage',
-  description: 'A full-stack village issue management platform with a Django-based backend that enables villagers to report public issues such as garbage, water leakage, drainage, electricity, and road damage. Implements role-based access, complaint management, REST APIs, database operations, status tracking, and email notifications.',
-  tech: [
-    'Python',
-    'Django',
-    'Django REST Framework',
-    'MySQL',
-    'React.js',
-    'Nodemailer',
-    'CSS'
-  ],
-  repoLink: 'https://github.com/skmsajid/fixmyvillage',
-  highlights: [
-    'Complaint Management',
-    'REST API Development',
-    'Role-Based Access',
-    'Email Notifications'
   ]
 },
     {
@@ -82,14 +80,16 @@ export default function Projects() {
               </div>
 
               <div className="project-links">
-                <a
-                  href={project.liveLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="project-link live-link"
-                >
-                  🔗 Live Demo
-                </a>
+               {project.id !== 1 && project.id !== 2 && (
+  <a
+    href={project.liveLink}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="project-link live-link"
+  >
+    🔗 Live Demo
+  </a>
+)}
                 <a
                   href={project.repoLink}
                   target="_blank"
